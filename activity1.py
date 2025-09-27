c9f68010d3c4cd7c7d448e3e9857d9ad18abdd03
scpviki <arun.letmein@gmail.com>
2025-09-27 13:18:17 +0400

    hh

---
+++ activity1.py
@@ -0,0 +1,12 @@
+x=5
+if (type(x)is int):
+    print("true")
+else:
+    print("false")
+
+y=20
+z=20
+if (y is z):
+    print("Both have the same identity")
+else:
+    print("Both have different identity")
